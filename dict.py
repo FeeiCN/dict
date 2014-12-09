24c01055b66f11809d94701a930dd59839309810
zhijie <zhijie@mogujie.com>
2014-12-09 13:13:07 +0800

    init

---
+++ dict.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import urllib2
+
+
+"""
+dict - Chinese/English Translation
+@author 止介(wufeifei@wufeifei.com)
+@date   2013.11.25
+"""
+
+
+class Dict:
+    key = '716426270'
+    keyFrom = 'wufeifei'
+    api = 'http://fanyi.youdao.com/openapi.do?keyfrom=wufeifei&key=716426270&type=data&doctype=json&version=1.1&q='
+    content = None
+
+    def __init__(self, argv):
+        if len(argv) == 1:
+            self.api = self.api + argv[0]
+            self.translate()
+        else:
+            print 'ERROR'
+
+    def translate(self):
+        content = urllib2.urlopen(self.api).read()
+        self.content = json.loads(content)
+        self.parse()
+
+    def parse(self):
+        code = self.content['errorCode']
+        if code == 0:  # Success
+            print '\033[1;31m################################### \033[0m'
+            print '\033[1;31m# \033[0m', self.content['query'], self.content['translation'][0], '(U:', self.content['basic']['us-phonetic'], 'E:', self.content['basic']['uk-phonetic'], ')'
+            for i in range(0, len(self.content['basic']['explains'])):
+                print '\033[1;31m# \033[0m', self.content['basic']['explains'][i]
+            print '\033[1;31m################################### \033[0m'
+            # Phrase
+            # for i in range(0, len(self.content['web'])):
+            #     print self.content['web'][i]['key'], ':'
+            #     for j in range(0, len(self.content['web'][i]['value'])):
+            #         print self.content['web'][i]['value'][j]
+        elif code == 20:  # Text to long
+            print 'WORD TO LONG'
+        elif code == 30:  # Trans error
+            print 'TRANSLATE ERROR'
+        elif code == 40:  # Don't support this language
+            print 'CAN\'T SUPPORT THIS LANGUAGE'
+        elif code == 50:  # Key failed
+            print 'KEY FAILED'
+        elif code == 60:  # Don't have this word
+            print 'DO\'T HAVE THIS WORD'
+
+if __name__ == '__main__':
+    Dict(sys.argv[1:])
